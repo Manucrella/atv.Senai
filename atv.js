d38f727b054d47ddf5d39dc289d721fc18f7baad
Manucrella <manuella.avelino@aluno.senai.br>
2025-03-16 12:55:14 -0300

    volta

--- atv.js
+++ atv.js
@@ -20,12 +20,3 @@ function iniTabNav() {
 }
 iniTabNav();
 
-function openMenu() {
-    document.getElementById("menu").style.left = "0";
-    document.getElementById("main").style.marginLeft = "250px";
-}
-
-function closeMenu() {
-    document.getElementById("menu").style.left = "-250px";
-    document.getElementById("main").style.marginLeft = "0";
-}
