31cad7cb9221827f9821576c504a67bf71af3c65
Manucrella <manuella.avelino@aluno.senai.br>
2025-03-16 12:53:29 -0300

    ff

--- atv.js
+++ atv.js
@@ -19,3 +19,13 @@ function iniTabNav() {
     }
 }
 iniTabNav();
+
+function openMenu() {
+    document.getElementById("menu").style.left = "0";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeMenu() {
+    document.getElementById("menu").style.left = "-250px";
+    document.getElementById("main").style.marginLeft = "0";
+}
